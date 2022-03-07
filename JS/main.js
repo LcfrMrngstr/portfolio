483aefb0cb4e083bb3b9716506c1a1e248145b3b
LcfrMrngstr <93025713+LcfrMrngstr@users.noreply.github.com>
2022-03-07 09:53:48 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+
+
+// Creating a repepating text background for the skills section
+const word_list = [ 'javascript', 'python', 'html', 'css', 'mongodb', 'java'];
+
+var count  = 0;
+var word_string = '';
+var this_word;
+
+while( count<200 ){
+
+    //Getting a random number
+    var rand_int = Math.floor((Math.random()*5)+1);
+    
+    //Getting a random word
+    this_word = word_list[rand_int-1];
+
+    //Adding the word to the string
+    word_string = word_string+ this_word + ' ';
+
+    count = count + 1
+};
+
+console.log(word_string)
+
+//Inserting the string into the html
+document.querySelector('#repeat_word_container').innerText = word_string
